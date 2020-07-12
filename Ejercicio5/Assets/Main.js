6b923ad5e6bb7e65dd5a5169919523f835aec1e4
RicardoM <66215032+RicardoMeran@users.noreply.github.com>
2020-07-12 12:06:13 -0400

    JS

---
+++ Ejercicio5/Assets/Main.js
@@ -0,0 +1,27 @@
+// console.log ("Hello World")
+
+let miVariable = "Ricardo";
+let mivariable = "Meran";
+
+console.log (miVariable);
+console.log (mivariable);
+
+// Ejemplos de constantes
+
+const MI_CONSTANTE = "Nombre de la Aplicacion";
+console.info (MI_CONSTANTE);
+
+// Ejemplo de tipos de datos
+
+let edad = 29;
+let devuelta = 15.29;
+let resultado = Math.round (15.29)
+console.log (resultado);
+console.log  (miVariable + mivariable)
+
+// Ejemplo variables Buleanas
+
+let variablebooleana = false;
+let variablebooleana2 = true;
+let variablebooleana3 = null;
+
